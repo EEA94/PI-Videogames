374a65af58c50e5db470777c624bdd218cddc331
EEA94 <14elias06andrada94@gmail.com>
2022-05-17 10:38:54 -0300

    last commit

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -9,12 +9,13 @@ export default function Card({name,image,genres,rating,id}){
                 <Link to={`/videogame/${id}`}>
                     <img className={styles.cardImg} src={image} alt={name} height='250px' width='200px'/>
                 </Link>
-            </div>
-            <div className={styles.info}>
+                <div className={styles.info}>
                 <b className={styles.name}>{name}</b><br/>
                 <b className={styles.titles}>Rating: </b><span>{rating}</span><br/>
                 <b className={styles.titles}>Genres: </b><span>{genres}.</span>
             </div>
+            </div>
+            
         </div>
     )
 }
